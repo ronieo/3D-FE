492d2e0cfc01b5661f91dffb5124e13066bcd549
dokimion24 <leecy3610@gmail.com>
2023-06-26 02:05:48 +0900

    feat: :sparkles: async 제거

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,8 +2,7 @@ import Layout from '@/components/common/Layout'
 import Cart from '@/components/cart/Cart'
 import CartInfo from '@/components/cart/CartInfo'
 
-
-export default async function CartPage() {
+export default function CartPage() {
   return (
     <Layout>
       <div className="flex min-h-[calc(100vh-7.2rem)] pt-[2.4rem] text-neutral-navy-100">
