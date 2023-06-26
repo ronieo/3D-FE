1a672d9c531c341ed6cf56003506690cd14fccd5
ronieo <romelzen50@gmail.com>
2023-06-26 22:25:19 +0900

    feat: :sparkles: yarn build 에러 수정

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -5,8 +5,7 @@ import SideNav from './sideNav/SideNav'
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen">
-      {/* @ts-expect-error Async Server Component */}
-      <SideNav />
+      {/* <SideNav /> */}
       <div className="ml-[24.4rem] h-full w-full">
         <Header />
         <main className="mt-[7.2rem] min-h-[calc(100vh-7.2rem)] bg-bg-2">{children}</main>
