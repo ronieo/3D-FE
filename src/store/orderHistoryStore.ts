9bce094fe7896eb66902ef5942a9974757e000c8
ronieo <romelzen50@gmail.com>
2023-06-29 05:28:31 +0900

    feat: :sparkles: 주문내역 리듀서 drawer제거

--- src/store/orderHistoryStore.ts
+++ src/store/orderHistoryStore.ts
@@ -1,15 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
-import drawerReducer from './drawerSlice'
 import clickedOrderHistoryReducer from './clickedOrderHistorySlice'
 import userReducer from './userSlice'
 import { store } from './store'
 
 export const orderHistoryStore = configureStore({
   reducer: {
-    // drawer: drawer/Reducer,
     clickedOrderHistory: clickedOrderHistoryReducer,
     user: userReducer,
   },
 })
 
-export type OrderHistoryState = ReturnType<typeof orderHistoryStore.getState>
+export type OrderHistoryState = ReturnType<typeof store.getState>
