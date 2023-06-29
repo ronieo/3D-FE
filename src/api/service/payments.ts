84968f0a19474ef61e59f6b8d93a29c70af29ea6
jiyoon29 <yoon__29@naver.com>
2023-06-29 16:35:33 +0900

    feat: :sparkles: payment api 추가

--- src/api/service/payments.ts
+++ src/api/service/payments.ts
@@ -1,11 +1,20 @@
 /**
  * 결제관련(결제하기, 결제내역, 주문내역 등) API
  */
-import { OrderResponse, PaymentPayload } from '../interface/payment'
+import {
+  OrderResponse,
+  PaymentPayload,
+} from '../interface/payment'
 import { axiosInstance } from '../axios'
 
 // 주문하기
 export const postPayment = async (T: PaymentPayload): Promise<OrderResponse> => {
   const res = await axiosInstance.post<OrderResponse>('/s/order', T)
+  return res.data
+}
+
+export const getOrderComplete = async (orderId: string) => {
+  const userId = Number(localStorage.getItem('userId'))
+  const res = await axiosInstance.get(`/s/user/${userId}/orders/${orderId}`)
   return res.data
 }
