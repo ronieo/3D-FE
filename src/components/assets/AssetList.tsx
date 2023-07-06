e2d4cad4e5a770ebcc49bfcc20a0ab8d77c4bc15
dokimion24 <leecy3610@gmail.com>
2023-07-06 20:48:53 +0900

    feat: :sparkles: props boolean 값에 따라 swiper 설정

--- src/components/assets/AssetList.tsx
+++ src/components/assets/AssetList.tsx
@@ -3,18 +3,40 @@
 import { Asset } from '@/api/interface/asset'
 import AssetItem from './AssetItem'
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
+
 interface Props {
   assets: Asset[] | undefined
+  swipeable: boolean
 }
 
-export default function AssetList({ assets }: Props) {
+export default function AssetList({ assets, swipeable }: Props) {
   if (!assets) return null
 
   return (
     <ul className="mb-[6.4rem] flex flex-wrap gap-2">
-      {assets.map((asset) => (
-        <AssetItem key={asset.assetId} asset={asset} />
-      ))}
+      {swipeable ? (
+        <Swiper
+          slidesPerView={2}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {assets.map((asset) => (
+            <SwiperSlide key={asset.assetId}>
+              <AssetItem asset={asset} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        assets.map((asset) => <AssetItem key={asset.assetId} asset={asset} />)
+      )}
     </ul>
   )
 }
