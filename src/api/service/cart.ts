3a380f135727a3f7d61f7c2bc7e124466ac290f2
jiyoon29 <yoon__29@naver.com>
2023-07-10 01:00:44 +0900

    feat: :sparkles: 장바구니 추가 api 작성

--- src/api/service/cart.ts
+++ src/api/service/cart.ts
@@ -1,7 +1,12 @@
 import { axiosInstance } from '../axios'
-import { CartDeleteRequest, CartDeleteResponse } from '../interface/cart'
+import {
+  CartAddRequest,
+  CartAddResponse,
+  CartDeleteRequest,
+  CartDeleteResponse,
+} from '../interface/cart'
 
-export const cartCount = async (user: number) => {
+export const cartCount = async (user: number): Promise<number> => {
   try {
     const res = await axiosInstance.get(`/s/user/${user}/cartCount`)
     if (res && res.data) {
@@ -22,4 +27,15 @@ export const cartList = async (user: number) => {
 export const cartDelete = async (T: CartDeleteRequest): Promise<CartDeleteResponse> => {
   const res = await axiosInstance.post<CartDeleteResponse>('/s/cart/delete', T)
   return res.data
+}
+
+export const cartAdd = async (request: CartAddRequest): Promise<CartAddResponse> => {
+  try {
+    const res = await axiosInstance.post<CartAddResponse>('/s/cart/add', request)
+    return res.data
+  } catch (error) {
+    // 오류 처리 로직
+    console.log('장바구니 아이템 추가 중 오류 발생:', error)
+    throw error
+  }
 }
